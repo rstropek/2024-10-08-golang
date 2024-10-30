9e5a2580200c2cc2fd48905f04d8825d208eb50e
Rainer <rainer@software-architects.at>
2024-10-30 11:36:08 +0100

    Custom error handling

--- hero-manager/cmd/api/routes.go
+++ hero-manager/cmd/api/routes.go
@@ -9,6 +9,9 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
 	router.HandlerFunc(http.MethodGet, "/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/heroes", app.createHeroHandler)
 	router.HandlerFunc(http.MethodGet, "/heroes/:id", app.showHeroHandler)
