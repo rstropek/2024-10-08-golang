2e172989c9e28765afbac855950e5fa37d07ce2d
Rainer <rainer@software-architects.at>
2024-10-30 11:02:05 +0100

    Add custom marshalling

--- hero-manager/internal/data/heroes.go
+++ hero-manager/internal/data/heroes.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"strings"
+	"time"
+)
 
 type Hero struct {
 	ID        int       `json:"id"`
@@ -10,4 +14,23 @@ type Hero struct {
 	RealName  string    `json:"realName,omitempty"`
 	Abilities []string  `json:"-"`
 	Version   int32     `json:"version"`
+}
+
+func (h Hero) MarshalJSON() ([]byte, error) {
+	var abilities string
+	if len(h.Abilities) > 0 {
+		abilities = strings.Join(h.Abilities, ", ")
+	}
+
+	type HeroAlias Hero
+
+	helper := struct {
+		HeroAlias
+		Abilities string `json:"abilities"`
+	}{
+		HeroAlias: HeroAlias(h),
+		Abilities: abilities,
+	}
+
+	return json.Marshal(helper)
 }
