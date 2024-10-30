ba6fdb7219782c47bf2a5fe908c169c01a77f90f
Rainer <rainer@software-architects.at>
2024-10-30 14:54:13 +0100

    Add migrations

--- hero-manager/cmd/api/main.go
+++ hero-manager/cmd/api/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -64,7 +66,10 @@ func openDB(cfg config) (*sql.DB, error) {
 		return nil, err
 	}
 
-	err = db.Ping()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
 	if err != nil {
 		return nil, err
 	}
