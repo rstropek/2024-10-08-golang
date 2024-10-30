8236163352c51a0147d81f2940b1255ea7ed8332
Rainer <rainer@software-architects.at>
2024-10-30 13:22:06 +0100

    Add panic in goroutine

--- hero-manager/cmd/api/routes.go
+++ hero-manager/cmd/api/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -18,6 +19,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/heroes/:id", app.showHeroHandler)
 	router.HandlerFunc(http.MethodGet, "/error", app.panicingHandler)
 	router.HandlerFunc(http.MethodGet, "/user", app.userHandler)
+	router.HandlerFunc(http.MethodGet, "/panic-in-goroutine", app.panicingHandlerInGoroutine)
 
 	return app.recoverPanic(app.authenticate(router))
 }
@@ -27,6 +29,21 @@ func (app *application) panicingHandler(w http.ResponseWriter, r *http.Request) 
 	y := 0
 	z := x / y
 	fmt.Fprintln(w, z)
+}
+
+func (app *application) panicingHandlerInGoroutine(w http.ResponseWriter, r *http.Request) {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		// NOTE: This panic will bring down our webserver!
+		// So: We need to recover from the panics if necessary!
+		
+		defer wg.Done()
+		panic("oh no")
+	}()
+	wg.Wait()
+
+	fmt.Fprintln(w, "done")
 }
 
 func (app *application) userHandler(w http.ResponseWriter, r *http.Request) {
